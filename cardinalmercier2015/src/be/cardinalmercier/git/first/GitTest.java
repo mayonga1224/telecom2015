ef80841440c370e17e3fa8c4856ee69b91fb420c
mayonga <mayongalouis1224@yahoo.fr>
2015-06-02 21:26:01 +0200

    Ceci est mon deuxième commit

--- cardinalmercier2015/src/be/cardinalmercier/git/first/GitTest.java
+++ cardinalmercier2015/src/be/cardinalmercier/git/first/GitTest.java
@@ -3,7 +3,8 @@ package be.cardinalmercier.git.first;
 public class GitTest {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("Voici un changement");
+		System.out.println("Git is fun");
 
 	}
 
